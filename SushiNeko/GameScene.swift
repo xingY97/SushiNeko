80c3462e9b14cab69e3b8ce72402edf557779dd0
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-02-02 19:58:52 -0800

    sushi pieces and chopsticks connected

--- SushiNeko/GameScene.swift
+++ SushiNeko/GameScene.swift
@@ -4,5 +4,17 @@ enum Side {
     case left, right, none
 }
 class GameScene: SKScene{
+    /* Game objects */
+    var sushiBasePiece: SushiPiece!
+
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        /* Connect game objects */
+        sushiBasePiece = (childNode(withName: "sushiBasePiece") as! SushiPiece)
+        /* Setup chopstick connections */
+        
+        sushiBasePiece.connectChopsticks()
+    }
     
+
 }
--- SushiNeko/SushiPiece.swift
+++ SushiNeko/SushiPiece.swift
@@ -13,16 +13,43 @@ class SushiPiece: SKSpriteNode {
     /* Chopsticks objects */
     var rightChopstick: SKSpriteNode!
     var leftChopstick: SKSpriteNode!
+    
+    var side: Side = .none {
+        didSet {
+            switch side {
+            case .left:
+                /* Show left chopstick */
+                leftChopstick.isHidden = false
+            case .right:
+                /* Show right chopstick */
+                rightChopstick.isHidden = false
+            case .none:
+                /* Hide all chopsticks */
+                leftChopstick.isHidden = true
+                rightChopstick.isHidden = true
+            }
+        }
+    }
 
     /* You are required to implement this for your subclass to work */
     override init(texture: SKTexture?, color: UIColor, size: CGSize) {
         super.init(texture: texture, color: color, size: size)
     }
 
+
     /* You are required to implement this for your subclass to work */
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    /* Sushi type */
 
+    func connectChopsticks() {
+        /* Connect our child chopstick nodes */
+        rightChopstick = (childNode(withName: "rightChopstick") as! SKSpriteNode)
+        leftChopstick = (childNode(withName: "leftChopstick") as! SKSpriteNode)
+        /* Set the default side */
+        side = .none
+
+    }
 }
 
