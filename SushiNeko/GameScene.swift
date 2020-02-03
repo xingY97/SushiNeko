611093072724cc65fec40ba1ab1165d0d44abdcf
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-02-03 14:17:03 -0800

    start page finished

--- SushiNeko/GameScene.swift
+++ SushiNeko/GameScene.swift
@@ -8,6 +8,8 @@ class GameScene: SKScene{
     var sushiBasePiece: SushiPiece!
     /* Cat Character */
     var character: Character!
+    /* Sushi tower array */
+    var sushiTower: [SushiPiece] = []
 
     override func didMove(to view: SKView) {
         super.didMove(to: view)
@@ -18,9 +20,38 @@ class GameScene: SKScene{
         
         sushiBasePiece.connectChopsticks()
         
-        /* Connect game objects */
-       
+        /* Manually stack the start of the tower */
+        addTowerPiece(side: .none)
+        addTowerPiece(side: .right)
+        
     }
     
+    func addTowerPiece(side: Side) {
+       /* Add a new sushi piece to the sushi tower */
 
+       /* Copy original sushi piece */
+       let newPiece = sushiBasePiece.copy() as! SushiPiece
+       newPiece.connectChopsticks()
+
+       /* Access last piece properties */
+       let lastPiece = sushiTower.last
+
+       /* Add on top of last piece, default on first piece */
+       let lastPosition = lastPiece?.position ?? sushiBasePiece.position
+       newPiece.position.x = lastPosition.x
+       newPiece.position.y = lastPosition.y + 55
+
+       /* Increment Z to ensure it's on top of the last piece, default on first piece*/
+       let lastZPosition = lastPiece?.zPosition ?? sushiBasePiece.zPosition
+       newPiece.zPosition = lastZPosition + 1
+
+       /* Set side */
+       newPiece.side = side
+
+       /* Add sushi to scene */
+       addChild(newPiece)
+
+       /* Add sushi piece to the sushi tower */
+       sushiTower.append(newPiece)
+    }
 }
